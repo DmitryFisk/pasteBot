0ceb709fb8c6479c1f2cf756c272b1294214402e
Даниїл Григор'єв <dengr1065@gmail.com>
2022-08-23 22:02:49 +0300

    доброго вечора ми з україни 😎 пес патрон 🐕 їбаш їх блять 🤬

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
+![GitHub repo file count (custom path)](https://img.shields.io/github/directory-file-count/DmitryFisk/pasteBot/raw?color=red&label=pastes)
+![last commit](https://img.shields.io/github/last-commit/DmitryFisk/pasteBot)
+![typescript](https://img.shields.io/github/languages/top/DmitryFisk/pasteBot)
+
 # Увага, щiткод!
-## Бот был сделан чисто поржать, поэтому прошу не поливать говном за код (а ветку v1 вообще не советую смотреть, версия в той ветке была сделана за десять минут)
-### Но если вы вдруг, каким-то необычайным образом захотели использовать эту залупу, то вот гайд
 
-# Установка
-## Перед использованием бота, сгенерируйте для него токен у [BotFather](https://t.me/BotFather) и включите для него инлайн командой `/setinline`
-### Затем, необходимо создать файл `config.json` и заполнить его так, как показано в `example-config.json`
+**Бот был сделан чисто поржать, поэтому прошу не поливать говном за код (а ветку v1 вообще не советую смотреть, версия в той ветке была сделана за десять минут)**
 
-1. ```git clone git@github.com:DmitryFisk/pasteBot.git```
-2. ```yarn install```
-3. ```yarn run```
+Но если вы вдруг, каким-то необычайным образом захотели использовать эту залупу, то вот гайд
 
-# todo:
-    - [x] ~~Перевести бота на TypeScript и убавить количество щиткода~~
-    - [x] ~~Сделать интерфейс для паст~~
-    - [x] Сделать выдачу айдишника пасты прямо в интерфейсе
-    - [x] Проебать первые два пункта
+## Установка
 
-# Авторы сея высера: [DmitryFisk](https://github.com/DmitryFisk), [dengr1065](https://github.com/dengr1065)
+**Перед использованием бота, сгенерируйте для него токен у [BotFather](https://t.me/BotFather) и включите для него инлайн командой `/setinline`**
+
+Затем, необходимо создать файл `config.json` и заполнить его так, как показано в `example-config.json`
+
+```
+git clone git@github.com:DmitryFisk/pasteBot.git
+yarn
+yarn start
+```
+
+## todo:
+
+-   [x] ~~Перевести бота на TypeScript и убавить количество щиткода~~
+-   [x] ~~Сделать интерфейс для паст~~
+-   [x] Сделать выдачу айдишника пасты прямо в интерфейсе
+-   [x] Проебать первые два пункта
+
+### Авторы сея высера: [DmitryFisk](https://github.com/DmitryFisk), [dengr1065](https://github.com/dengr1065)
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,53 @@
-import { Telegraf } from "telegraf";
-import { generatePastes } from "./utils";
-import { token } from "./config";
+import { Context, Telegraf } from "telegraf";
+import { token } from "./config.json";
+import { Paste } from "./paste";
+import { readdir } from "fs/promises";
+import { resolve } from "path";
+import { InlineQueryResult, Update } from "telegraf/typings/core/types/typegram";
 
-const bot = new Telegraf(token);
-bot.pastes = [];
+class PasteBot extends Telegraf {
+    pastes: Paste[] = [];
 
-bot.on("inline_query", async (ctx) => {
-    const filter: object = bot.pastes.filter(p => p.type.toUpperCase().includes(ctx.inlineQuery.query.toUpperCase()));
-    
-    ctx.answerInlineQuery(filter.map(p => {
-        return {
-            id: p.id,
-            title: `Паста про ${p.type.toLowerCase()}`,
-            description: p.description,
-            type: "article",
-            input_message_content: {
-                message_text: p.content.substr(0, 4090)
-            }
+    constructor() {
+        super(token);
+    }
+
+    async launch() {
+        await super.launch();
+
+        const files = await readdir("./raw/");
+        for (const file of files) {
+            const path = resolve("./raw", file);
+            const paste = await Paste.fromFile(path);
+
+            this.pastes.push(paste);
         }
-    }));
-});
 
-bot.launch().then(async () => {
-    console.log("Bot started");
+        this.on("inline_query", this.onInlineQuery.bind(this));
+    }
 
-    await generatePastes(bot);
-});
+    async onInlineQuery(ctx: Context<Update>) {
+        const query = ctx.inlineQuery!!.query;
+        const matching = this.pastes.filter((paste) => paste.matches(query));
+
+        const inlineResults: InlineQueryResult[] = matching.map((paste) => {
+            return {
+                id: this.pastes.indexOf(paste).toString(),
+                title: paste.title,
+                description: paste.text,
+                type: "article",
+                video_url: null,
+                input_message_content: {
+                    message_text: paste.text.slice(0, 4090)
+                }
+            };
+        });
+
+        await ctx.answerInlineQuery(inlineResults);
+    }
+}
+
+const bot = new PasteBot();
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
